c6e111e9ba426000cf5fa1dcdc374f1b2a595c52
caiodelucena <caiodelucena6@gmail.com>
2021-11-21 22:40:55 -0300

    Alteração na model Category, relação has_many com artigos e criação do metódo para definir a url dos artigos de cada categoria

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  has_many :articles
+
   before_save :set_url
 
   validates :name, uniqueness: { case_sensitive: false }, presence: true
@@ -7,11 +9,13 @@ class Category < ApplicationRecord
   private
 
   def set_url
-    if self.new_record? && Category.count > 0
-      @last_id_category = Category.last.id
-      self.url = "http://localhost:3000/api/v1/categories/#{@last_id_category + 1}"
-    else
-      self.url = "http://localhost:3000/api/v1/categories/#{1}"
+    if self.new_record?
+      if Category.all.present?
+        @last_id_category = Category.last.id
+        self.url = "http://localhost:3000/api/v1/categories/#{@last_id_category + 1}"
+      else
+        self.url = "http://localhost:3000/api/v1/categories/#{1}"
+      end
     end
   end
 end
